27e538b26167a62315cd817745275334f9927893
Justin Seymour <justin@3wmholdings.com>
2018-03-08 09:49:33 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,82 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/swag-shop');
+
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/post', function(req, res) {
+    var product = new Product();
+    product.title = req.body.title;
+    product.price = req.body.price;
+    product.save(function(err, savedProduct) {
+        if (err) {
+            res.status(500).send({error:"Could not save the product"});
+        } else {
+            res.status(200).send(savedProduct);
+        }
+    });
+});
+
+app.get('/get', function(req, res) {
+    Product.find({}, function(err, products) {
+        if (err) {
+            res.status(500).send({error: "Could not find products in database"});
+        } else {
+            res.send(products);
+        }
+    });
+});
+
+app.get('/wishlist', function(req, res) {
+    WishList.find({}).populate({path: 'products', model: 'Product'}).exec(function(err, wishLists) {
+        
+        if (err) {
+            res.status(500).send({error: "Could not fetch wishlists"});
+        } else {
+            res.status(200).send(wishLists);
+        }
+    })
+});
+
+app.put('/wishlist/product/add', function(req, res) {
+    Product.findOne({_id: req.body.productId}, function(err, product) {
+        if (err) {
+            res.status(500).send({error: "Could not add item to wishList"});
+        } else {
+            WishList.update({_id:req.body.wishListId}, {$addToSet:{products: product._id}}, function(err, wishList) {
+                if (err) {
+                    res.status(500).send({error:"Could not add item to wishlist"});
+                } else {
+                    res.send("Successfully added product to wishlist");
+                }
+            });
+        }
+    })
+});
+
+app.post('/wishlist', function(req, res) {
+    var wishList = new WishList();
+    wishList.title = req.body.title;
+
+    wishList.save(function(err, newWishList) {
+        if (err) {
+            res.status(500).send({error: "Could not create wishlist"});
+        } else {
+            res.send(newWishList);
+        };
+    });
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Swag Shop API running on port 3000...");
+});
